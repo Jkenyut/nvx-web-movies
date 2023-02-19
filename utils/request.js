b4227103673bdd2fdf08341ca6302dc06da7e920
Jkenyut <satrianursaputro06@gmail.com>
2023-02-19 11:54:18 +0700

    commit front-end

---
+++ utils/request.js
@@ -0,0 +1,26 @@
+const API_KEY = process.env.API_KEY;
+
+const Category = {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}`,
+  },
+  fetchTV: {
+    title: "Action",
+    url: `/trending/tv/day?api_key=${API_KEY}`,
+  },
+  fetchTopRatedMovie: {
+    title: "Top Rated Movie",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  },
+  fetchTopRatedTV: {
+    title: "Top Event TV",
+    url: `/tv/top_rated?api_key=${API_KEY}&languange=en-US&page=1`,
+  },
+  fetchTopRatedPopuler: {
+    title: "Top Event Populer",
+    url: `/movie/popular?api_key=${API_KEY}&languange=en-US&page=1`,
+  },
+};
+
+export default Category;
